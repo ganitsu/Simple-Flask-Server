27ff15e7a62b96502bd70ac51557f94f1f7d5000
nitsuga2 <giulianiagustin7@gmail.com>
2024-09-19 17:42:57 -0300

    mejorado

--- webhook_server.py
+++ webhook_server.py
@@ -1,35 +1,58 @@
 from flask import Flask, request, abort
 import subprocess
 import os
+import hmac
+import hashlib
+from webhookconfig import *
 
 app = Flask(__name__)
 
-# Configura la ruta de tu repositorio
-REPO_DIR = '/path/to/your/repo'
+
+def verify_signature(request):
+    """
+    Verifica la firma del webhook usando la clave secreta.
+    """
+    signature = request.headers.get('X-Hub-Signature-256')
+    if not signature:
+        return False
+
+    # Calcula la firma esperada
+    digest = hmac.new(SECRET, request.data, hashlib.sha256).hexdigest()
+    expected_signature = f'sha256={digest}'
+
+    return hmac.compare_digest(expected_signature, signature)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    if request.method == 'POST':
-        # Verifica el contenido del payload si es necesario
-        data = request.json
+    """
+    Maneja las solicitudes POST del webhook.
+    """
+    # Verifica la firma del webhook
+    if USE_SECRET and not verify_signature(request):
+        abort(400, 'Invalid signature')
 
-        # Cambia al directorio del repositorio
+    # Verifica que el repositorio en el payload coincide con el esperado
+    data = request.json
+    # repository_name = data.get('repository', {}).get('name')
+    # if repository_name != EXPECTED_REPO_NAME:
+    #     abort(400, 'Not the expected repository')
+
+    # Cambia al directorio del repositorio
+    
+    # Actualiza el repositorio
+    try:
         os.chdir(REPO_DIR)
         
-        # Actualiza el repositorio
-        try:
-            subprocess.check_call(['git', 'pull'])
-            print("Repositorio actualizado.")
-            
-            # Ejecuta tu script o reinicia el programa
-            subprocess.check_call(['python3', 'your_script.py'])
-            print("Programa reiniciado.")
-        except subprocess.CalledProcessError as e:
-            print(f"Error al ejecutar el comando: {e}")
+        subprocess.check_call(["git", "pull"])
+        print("Repositorio actualizado.")
+        
+        # Ejecuta tu script o reinicia el programa
+        subprocess.check_call(RESTART_CALL)
+        print("Programa reiniciado.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error al ejecutar el comando: {e}")
 
-        return '', 200
-    else:
-        abort(400)
+    return '', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
---
+++ webhookconfig.py
@@ -0,0 +1,9 @@
+REPO_DIR = "C:/Users/HP/Documents/SimpleFlaskServer/Simple-Flask-Server"
+SECRET = b'VamoooEsMiprimerweebhok1'
+EXPECTED_REPO_NAME = 'expected-repo-name'
+
+USE_SECRET = False
+
+
+RESTART_CALL = ['python', '--version']
+
